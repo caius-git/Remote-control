24c24e6955f83255748783576db3c8f473f02f35
Caius Juvonen <caius@localhost.localdomain>
2021-03-03 18:19:17 +0200

    Added encryption

--- RemoteControlSoftware/server.py
+++ RemoteControlSoftware/server.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
 import socket
+import ssl
 
 HOST = "10.0.2.7"
 PORT = 6666
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
-    s.listen()
+    wrapS = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_TLSv1, server_side=True,
+    keyfile="/home/caius/key_and_cert_files/key.pem", certfile="/home/caius/key_and_cert_files/cert.pem")
+    wrapS.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    wrapS.bind((HOST, PORT))
+    wrapS.listen()
     print("[+] Listening for connections")
-    conn, addr = s.accept()
+    conn, addr = wrapS.accept()
     print("[+] Connection established with target ", addr)
     while True:
         command = input(">> ")
